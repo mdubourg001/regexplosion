ecca9ecac6c1e89bcfd8500607b1b14196211638
mdubourg <mdubourg@akema.fr>
2018-04-25 15:51:04 +0200

    added load before rendering

--- static/js/application.js
+++ static/js/application.js
@@ -1,17 +1,21 @@
-let model = new Model();
+$(window).bind("load", function () {
+    let model = new Model();
 
 // remove dead alerts every 15 seconds
-setInterval(function () {
-    if (model.alerts.length > 0)
-        model.remove_dead_alerts();
-}, 5000);
+    setInterval(function () {
+        if (model.alerts.length > 0)
+            model.remove_dead_alerts();
+    }, 5000);
 
 
-new Vue({
-    delimiters: ['[[', ']]'],
-    el: '#app',
-    data: {
-        ascii_table: ASCII_CHARS,
-        model: model
-    }
+    new Vue({
+        delimiters: ['[[', ']]'],
+        el: '#app',
+        data: {
+            ascii_table: ASCII_CHARS,
+            model: model
+        }
+    });
 });
+
+
